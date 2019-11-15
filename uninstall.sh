a142d3dc3d9999e4d092fdb4fecdebe39427f473
Ishmael Desjarlais <info@onepacificave.com>
2019-11-14 20:55:06 -0500

    Add line to remove the plugin folder and files when uninstalling

--- uninstall.sh
+++ uninstall.sh
@@ -12,6 +12,8 @@ echo "Removing the Themely cPanel Plugin"
 echo "-------------------------------------------------"
 # Unregister the plugin
 /usr/local/cpanel/scripts/uninstall_plugin /usr/local/cpanel/base/frontend/paper_lantern/themely/themely.tar.gz --theme=paper_lantern
+# Remove the plugin folder & files
+rm -rf /usr/local/cpanel/base/frontend/paper_lantern/themely
 # Remove the uninstall.sh file from root directory
 rm uninstall.sh
 echo "----------------------------"
