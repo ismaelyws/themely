a4a29264fe1c7028c7dc3d456bbaa4f5757572cb
Ishmael Desjarlais <info@onepacificave.com>
2019-11-14 20:31:57 -0500

    Changed name to Themely

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # SCRIPT: install.sh
-# PURPOSE: Install the One-Click WP Installer plugin into cPanel
+# PURPOSE: Install the Themely plugin into cPanel
 # AUTHOR: Ishmael 'Hans' Desjarlais <ismaelyws@gmail.com>
 # INSTALLATION INSTRUCTIONS:
 # wget -N https://bitbucket.org/ismaelyws/themely/raw/efeaffe91d4beca313baa69f29aa0ce9a178503c/install.sh
@@ -8,29 +8,28 @@
 # ./install.sh
 clear
 echo "-----------------------------------------------"
-echo "Installing One-Click WP Installer cPanel Plugin"
+echo "Installing Themely cPanel Plugin"
 echo "-----------------------------------------------"
 # Create the directory for the plugin
-mkdir -p /usr/local/cpanel/base/frontend/paper_lantern/oneclick-wp-installer
+mkdir -p /usr/local/cpanel/base/frontend/paper_lantern/themely
 # Get plugin files from repository
-curl -s http://172.105.97.228/ocwpi.tar.gz > /root/ocwpi.tar.gz
+curl -s http://172.105.97.228/themely.tar.gz > /root/themely.tar.gz
 # Uncompress the archive
-tar xzf ocwpi.tar.gz
-# Move files to /usr/local/cpanel/base/frontend/paper_lantern/oneclick-wp-installer directory
-mv /root/index.live.php /usr/local/cpanel/base/frontend/paper_lantern/oneclick-wp-installer
-mv /root/process.php /usr/local/cpanel/base/frontend/paper_lantern/oneclick-wp-installer
-mv /root/search.php /usr/local/cpanel/base/frontend/paper_lantern/oneclick-wp-installer
-mv /root/dbconfig.php /usr/local/cpanel/base/frontend/paper_lantern/oneclick-wp-installer
-mv /root/style.css /usr/local/cpanel/base/frontend/paper_lantern/oneclick-wp-installer
-mv /root/script.js /usr/local/cpanel/base/frontend/paper_lantern/oneclick-wp-installer
-mv /root/wordpress-logo.png /usr/local/cpanel/base/frontend/paper_lantern/oneclick-wp-installer
-mv /root/CHANGELOG.md /usr/local/cpanel/base/frontend/paper_lantern/oneclick-wp-installer
-mv /root/LICENSE.md /usr/local/cpanel/base/frontend/paper_lantern/oneclick-wp-installer
-mv /root/oneclick-wp-installer.tar.gz /usr/local/cpanel/base/frontend/paper_lantern/oneclick-wp-installer
+tar xzf themely.tar.gz
+# Move files to /usr/local/cpanel/base/frontend/paper_lantern/themely directory
+mv /root/index.live.php /usr/local/cpanel/base/frontend/paper_lantern/themely
+mv /root/search.live.php /usr/local/cpanel/base/frontend/paper_lantern/themely
+mv /root/config.ini /usr/local/cpanel/base/frontend/paper_lantern/themely
+mv /root/style.css /usr/local/cpanel/base/frontend/paper_lantern/themely
+mv /root/script.js /usr/local/cpanel/base/frontend/paper_lantern/themely
+mv /root/wordpress-logo.png /usr/local/cpanel/base/frontend/paper_lantern/themely
+mv /root/CHANGELOG.md /usr/local/cpanel/base/frontend/paper_lantern/themely
+mv /root/LICENSE.md /usr/local/cpanel/base/frontend/paper_lantern/themely
+mv /root/themely.tar.gz /usr/local/cpanel/base/frontend/paper_lantern/themely
 # Install the plugin (which also places the png image in the proper location)
-/usr/local/cpanel/scripts/install_plugin /usr/local/cpanel/base/frontend/paper_lantern/oneclick-wp-installer/oneclick-wp-installer.tar.gz
-# Remove install.sh and ocwpi.tar.gz files from root directory
-rm ocwpi.tar.gz
+/usr/local/cpanel/scripts/install_plugin /usr/local/cpanel/base/frontend/paper_lantern/themely/themely.tar.gz
+# Remove install.sh and themely.tar.gz files from root directory
+rm themely.tar.gz
 rm install.sh
 echo "------------------------------------"
 echo "Installation successfully completed!"
--- uninstall.sh
+++ uninstall.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # SCRIPT: uninstall.sh
-# PURPOSE: Remove the One-Click WP Installer plugin from cPanel
+# PURPOSE: Remove the Themely plugin from cPanel
 # AUTHOR: Ishmael 'Hans' Desjarlais <ismaelyws@gmail.com>
 # REMOVAL INSTRUCTIONS:
 # wget -N https://bitbucket.org/ismaelyws/themely/raw/6d0f897d43b3ad198ab7c8803d4a7c2eb9effa64/uninstall.sh
@@ -8,10 +8,10 @@
 # ./uninstall.sh
 clear
 echo "-------------------------------------------------"
-echo "Removing the One-Click WP Installer cPanel Plugin"
+echo "Removing the Themely cPanel Plugin"
 echo "-------------------------------------------------"
 # Unregister the plugin
-/usr/local/cpanel/scripts/uninstall_plugin /usr/local/cpanel/base/frontend/paper_lantern/oneclick-wp-installer/oneclick-wp-installer.tar.gz --theme=paper_lantern
+/usr/local/cpanel/scripts/uninstall_plugin /usr/local/cpanel/base/frontend/paper_lantern/themely/themely.tar.gz --theme=paper_lantern
 # Remove the uninstall.sh file from root directory
 rm uninstall.sh
 echo "----------------------------"
