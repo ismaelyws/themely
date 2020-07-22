c53fad0a75686f502cb17ea8a06808cacfea9879
Hans Desjarlais <ismaelyws@gmail.com>
2020-07-22 17:06:08 -0400

    no message

--- install.sh
+++ install.sh
@@ -3,6 +3,12 @@
 # PURPOSE: Install the Themely cPanel/WHM plugin
 # AUTHOR: Ishmael 'Hans' Desjarlais <ismaelyws@gmail.com>
 clear
+if [ ! -d /usr/local/cpanel ]; then
+	echo "------------------------------------"
+	echo "cPanel/WHM is not installed on your server."
+	echo "------------------------------------"
+	exit 0
+fi
 echo "-----------------------------------------------"
 echo "Installing Themely cPanel/WHM Plugin"
 echo "-----------------------------------------------"
@@ -43,20 +49,27 @@ mv /root/style.css /usr/local/cpanel/base/frontend/paper_lantern/themely
 mv /root/script.js /usr/local/cpanel/base/frontend/paper_lantern/themely
 mv /root/wordpress-logo.png /usr/local/cpanel/base/frontend/paper_lantern/themely
 mv /root/LICENSE.md /usr/local/cpanel/base/frontend/paper_lantern/themely
-mv /root/locale.tgz /usr/local/cpanel/base/frontend/paper_lantern/themely
+mv /root/es_ES.tgz /usr/local/cpanel/base/frontend/paper_lantern/themely/locale/es_ES/LC_MESSAGES
+mv /root/fr_FR.tgz /usr/local/cpanel/base/frontend/paper_lantern/themely/locale/fr_FR/LC_MESSAGES
 mv /root/themely.tar.gz /usr/local/cpanel/base/frontend/paper_lantern/themely
 mv /root/themely.conf /usr/local/cpanel/whostmgr/docroot/templates/themely
 mv /root/whm.php /usr/local/cpanel/whostmgr/docroot/templates/themely
-# Uncompress locale zip file
-tar -zxvf /usr/local/cpanel/base/frontend/paper_lantern/themely/locale.tgz
+# Uncompress locale zip files
+cd /usr/local/cpanel/base/frontend/paper_lantern/themely/locale/es_ES/LC_MESSAGES
+tar -zxvf es_ES.tgz
+cd /usr/local/cpanel/base/frontend/paper_lantern/themely/locale/fr_FR/LC_MESSAGES
+tar -zxvf fr_FR.tgz
+cd /root/
 # Install cPanel plugin
 /usr/local/cpanel/scripts/install_plugin /usr/local/cpanel/base/frontend/paper_lantern/themely/themely.tar.gz
 # Register WHM plugin with AppConfig
 /usr/local/cpanel/bin/register_appconfig /usr/local/cpanel/whostmgr/docroot/templates/themely/themely.conf
 # Remove installation files
 rm archive.tgz
-rm /usr/local/cpanel/base/frontend/paper_lantern/themely/locale.tgz
-rm install.sh
+rm /usr/local/cpanel/base/frontend/paper_lantern/themely/locale/es_ES/LC_MESSAGES/es_ES.tgz
+rm /usr/local/cpanel/base/frontend/paper_lantern/themely/locale/fr_FR/LC_MESSAGES/fr_FR.tgz
+rm update.sh
 echo "------------------------------------"
 echo "Themely successfully installed! Proceed to WHM > Plugins > Themely and enter your license key to activate the server."
 echo "------------------------------------"
+exit 0
